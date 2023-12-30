79e3596f50c5b0c297a0126c9c7f1ad4cad91651
eduardocaas <eduardofc.dev@gmail.com>
2023-12-30 12:56:22 -0300

    Console art and menu implemented

--- Screens/RoleScreen/MenuRoleScreen.cs
+++ Screens/RoleScreen/MenuRoleScreen.cs
@@ -6,6 +6,29 @@ public static class MenuRoleScreen
 {
     public static void Load(SqlConnection connection)
     {
-        
+        Console.Clear();
+        Console.WriteLine(@"
+              _____   ____  _      ______  _____ 
+             |  __ \ / __ \| |    |  ____|/ ____|
+             | |__) | |  | | |    | |__  | (___  
+             |  _  /| |  | | |    |  __|  \___ \ 
+             | | \ \| |__| | |____| |____ ____) |
+             |_|  \_\\____/|______|______|_____/                                                                 
+        ");
+
+        Console.Write(" >> 1: Show roles \n >> 2: Add role to user \n " + 
+                      ">> 3: Numbers of users per role \n >> 4: Delete role \n >> 5: Return \n >> ");
+
+        short option = short.Parse(Console.ReadLine());
+
+        switch (option)
+        {
+            case 1: ListRoleScreen.Load(connection); break;
+            case 2: Load(connection); break;
+            case 3: Load(connection); break;
+            case 4: Load(connection); break;
+            case 5: MenuScreen.Load(connection); break;
+            default: Load(connection); break;
+        }
     }
 }
