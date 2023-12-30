97e99c969651ae0448601b1aacd1e7a0185a05f6
eduardocaas <eduardofc.dev@gmail.com>
2023-12-30 19:18:01 -0300

    List tag call method

--- Screens/TagScreen/MenuTagScreen.cs
+++ Screens/TagScreen/MenuTagScreen.cs
@@ -1,21 +1,26 @@
-﻿namespace Blog.Screens.TagScreen;
+﻿using Microsoft.Data.SqlClient;
+
+namespace Blog.Screens.TagScreen;
 
 public static class MenuTagScreen
 {
-    public static void Load()
+    public static void Load(SqlConnection connection)
     {
         Console.Clear();
         Console.WriteLine("-------- MENU TAG --------");
-        Console.Write(" >> 1: Show tags \n >> 2: Create tag \n >> 3: Update tag \n >> 4: Delete tag \n >> ");
+        Console.Write(" >> 1: Show tags \n >> 2: Create tag \n >> 3: Add tag to post" +
+                      " \n >> 4: Delete tag \n >> 5: Return \n >> ");
+        
         short option = short.Parse(Console.ReadLine());
         
         switch (option)
         {
-            case 1: Load(); break;
-            case 2: Load(); break;
-            case 3: Load(); break;
-            case 4: Load(); break;
-            default: Load(); break;
+            case 1: ListTagScreen.Load(connection); break;
+            case 2: Load(connection); break;
+            case 3: Load(connection); break;
+            case 4: Load(connection); break;
+            case 5: MenuScreen.Load(connection); break;
+            default: Load(connection); break;
         }
     }
 }
