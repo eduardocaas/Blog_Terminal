f58b3fbf01b53d7abdb84ac1762df7fcb91f6860
eduardocaas <eduardofc.dev@gmail.com>
2023-12-30 19:17:50 -0300

    Load and Read methods implemented

--- Screens/TagScreen/ListTagScreen.cs
+++ Screens/TagScreen/ListTagScreen.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Blog.Models;
+using Blog.Repositories;
+using Microsoft.Data.SqlClient;
 
 namespace Blog.Screens.TagScreen;
 
@@ -6,11 +8,27 @@ public static class ListTagScreen
 {
     public static void Load(SqlConnection connection)
     {
-        
+        Console.Clear();
+        Console.WriteLine(@"
+              _______       _____       _      _____  _____ _______ 
+             |__   __|/\   / ____|     | |    |_   _|/ ____|__   __|
+                | |  /  \ | |  __      | |      | | | (___    | |   
+                | | / /\ \| | |_ |     | |      | |  \___ \   | |   
+                | |/ ____ \ |__| |     | |____ _| |_ ____) |  | |   
+                |_/_/    \_\_____|     |______|_____|_____/   |_|                                                   
+        ");
+        Read(connection);
+        Console.Write("\n\n >> Press key to return to tag menu: ");
+        Console.ReadKey();
+        MenuTagScreen.Load(connection);
     }
 
     public static void Read(SqlConnection connection)
     {
-        
+        Repository<Tag> repository = new Repository<Tag>(connection);
+        IEnumerable<Tag> items = repository.Get();
+
+        foreach (Tag item in items)
+            Console.WriteLine($"{item.Id} - {item.Name}");
     }
 }
