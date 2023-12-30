42e77d654f7eeb9b1a4d4e7c4355e5c9a1f3f23b
eduardocaas <eduardofc.dev@gmail.com>
2023-12-30 19:44:00 -0300

    Add CreateTagScreen.cs

---
+++ Screens/TagScreen/CreateTagScreen.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Blog.Screens.TagScreen;
+
+public static class CreateTagScreen
+{
+    public static void Load(SqlConnection connection)
+    {
+        
+    }
+
+    public static void Create(SqlConnection connection)
+    {
+        
+    }
+}
