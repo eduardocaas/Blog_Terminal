1e3f9e632117e66d34b3eb8bf7384613f8e489c3
eduardocaas <eduardofc.dev@gmail.com>
2023-12-30 19:09:44 -0300

    Methods Load and Read

---
+++ Screens/TagScreen/ListTagScreen.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Blog.Screens.TagScreen;
+
+public static class ListTagScreen
+{
+    public static void Load(SqlConnection connection)
+    {
+        
+    }
+
+    public static void Read(SqlConnection connection)
+    {
+        
+    }
+}
