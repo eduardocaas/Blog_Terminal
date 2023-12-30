2a5b0b2db3281e0b30dc5b903063f7342d06cfa3
eduardocaas <eduardofc.dev@gmail.com>
2023-12-30 19:47:44 -0300

    Implemented method Load

--- Screens/TagScreen/CreateTagScreen.cs
+++ Screens/TagScreen/CreateTagScreen.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Blog.Screens.TagScreen;
 
@@ -6,10 +7,25 @@ public static class CreateTagScreen
 {
     public static void Load(SqlConnection connection)
     {
+        Console.Clear();
+        Console.WriteLine(@"
+               _____  _____   ______         _______  ______      _______         _____ 
+              / ____||  __ \ |  ____|    /\ |__   __||  ____|    |__   __| /\    / ____|
+             | |     | |__) || |__      /  \   | |   | |__          | |   /  \  | |  __ 
+             | |     |  _  / |  __|    / /\ \  | |   |  __|         | |  / /\ \ | | |_ |
+             | |____ | | \ \ | |____  / ____ \ | |   | |____        | | / ____ \| |__| |
+              \_____||_|  \_\|______|/_/    \_\|_|   |______|       |_|/_/    \_\\_____|                                                      
+        ");
+        Console.Write("\n >> Name: ");
+        string name = Console.ReadLine();
+        if (name.IsNullOrEmpty()) Load(connection);
+        Console.Write("\n >> Slug: ");
+        string slug = Console.ReadLine();
         
+        Create(connection, name, slug);
     }
 
-    public static void Create(SqlConnection connection)
+    public static void Create(SqlConnection connection, string name, string slug)
     {
         
     }
