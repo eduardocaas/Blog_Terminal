5d04d6ae482a011d728661e06c144f4fdbf19519
eduardocaas <eduardofc.dev@gmail.com>
2023-12-31 19:08:35 -0300

    Add DeleteUserScreen.cs

---
+++ Screens/UserScreen/DeleteUserScreen.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Blog.Screens.UserScreen;
+
+public static class DeleteUserScreen
+{
+    public static void Load(SqlConnection connection)
+    {
+        Console.Clear();
+        var art = @"
+              _____   ______  _       ______  _______  ______      _    _   _____  ______  _____  
+             |  __ \ |  ____|| |     |  ____||__   __||  ____|    | |  | | / ____||  ____||  __ \ 
+             | |  | || |__   | |     | |__      | |   | |__       | |  | || (___  | |__   | |__) |
+             | |  | ||  __|  | |     |  __|     | |   |  __|      | |  | | \___ \ |  __|  |  _  / 
+             | |__| || |____ | |____ | |____    | |   | |____     | |__| | ____) || |____ | | \ \ 
+             |_____/ |______||______||______|   |_|   |______|     \____/ |_____/ |______||_|  \_\
+        ";
+        Console.WriteLine(art);
+        ConsoleKey key;
+        do
+        {
+            Console.Write("Press 'I' to delete user by Id or 'N' to delete user by Name or 'R' to return: ");
+            key = Console.ReadKey().Key;
+            if (key == ConsoleKey.R) MenuUserScreen.Load(connection); break;
+            
+        } while (key != ConsoleKey.I && key != ConsoleKey.N && key != ConsoleKey.R);
+        
+        
+    }
+}
