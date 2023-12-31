fb79bff1accd2c54982dffe660b632b61e0d1e39
eduardocaas <eduardofc.dev@gmail.com>
2023-12-31 19:37:25 -0300

    Delete method implemented

--- Screens/UserScreen/DeleteUserScreen.cs
+++ Screens/UserScreen/DeleteUserScreen.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Blog.Models;
+using Blog.Repositories;
+using Microsoft.Data.SqlClient;
 
 namespace Blog.Screens.UserScreen;
 
@@ -19,17 +21,55 @@ public static class DeleteUserScreen
         ConsoleKey key;
         do
         {
-            Console.Write("Press 'I' to delete user by Id or 'N' to delete user by Name or 'R' to return: ");
+            Console.Write("Press 'I' to delete user by Id or 'E' to delete user by Email or 'R' to return: ");
             key = Console.ReadKey().Key;
             if (key == ConsoleKey.R) { MenuUserScreen.Load(connection); break; }
-            if (key == ConsoleKey.I) continue;
-            if (key == ConsoleKey.N) continue;
+            if (key == ConsoleKey.I) Delete(connection, 1);
+            if (key == ConsoleKey.E) Delete(connection, 2);
             
-        } while (key != ConsoleKey.I && key != ConsoleKey.N && key != ConsoleKey.R);
+        } while (key != ConsoleKey.I && key != ConsoleKey.E && key != ConsoleKey.R);
     }
 
     public static void Delete(SqlConnection connection, short option)
     {
-        
+        UserRepository repository = new UserRepository(connection);
+
+        try
+        {
+
+            if (option == 1)
+            {
+                Console.Write("\n\n >> Id: ");
+                int id = int.Parse(Console.ReadLine());
+
+                User user = repository.Get(id);
+                repository.Delete(user);
+
+                Console.WriteLine(
+                    "\n |      User deleted with success!      | \n\n >> Press key to return to user menu: ");
+                Console.ReadKey();
+                MenuUserScreen.Load(connection);
+            }
+
+            else if (option == 2)
+            {
+                Console.Write("\n\n >> Email: ");
+                string email = Console.ReadLine();
+                repository.DeleteByEmail(email);
+
+                Console.WriteLine(
+                    "\n |      User deleted with success!      | \n\n >> Press key to return to user menu: ");
+                Console.ReadKey();
+                MenuUserScreen.Load(connection);
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error! {e.Message}");
+            Console.WriteLine(" >> Press key to return to user delete: ");
+            Console.ReadKey();
+            Load(connection);
+        }
     }
 }
