574d8c868ca7f05e4183277e7f8c8fd83e76aeb4
eduardocaas <eduardofc.dev@gmail.com>
2024-01-01 13:07:44 -0300

    Refactored menu

--- Screens/MenuScreen.cs
+++ Screens/MenuScreen.cs
@@ -9,27 +9,36 @@ public static class MenuScreen
 {
     public static void Load(SqlConnection connection)
     {
-        Console.Clear();
-        Console.WriteLine(@"         
+        var art =@"         
               __  __  ______  _   _  _    _ 
              |  \/  ||  ____|| \ | || |  | |
              | \  / || |__   |  \| || |  | |
              | |\/| ||  __|  | . ` || |  | |
              | |  | || |____ | |\  || |__| |
              |_|  |_||______||_| \_| \____/                                                                                
-        ");
-        Console.Write(" >> 1: Users \n >> 2: Posts \n >> 3: Roles \n >> 4: Tags \n >> 5: Categories \n >> 6: Exit \n >> ");
-        short option = short.Parse(Console.ReadLine());
-
-        switch (option)
+        ";
+        
+        ConsoleKey key;
+        do
         {
-            case 1: MenuUserScreen.Load(connection); break;
-            case 2: Load(connection); break;
-            case 3: MenuRoleScreen.Load(connection); break;
-            case 4: MenuTagScreen.Load(connection); break;
-            case 5: Load(connection); break;
-            case 6: Console.Clear(); break;
-            default: Load(connection); break;
-        }
+            Console.Clear();
+            Console.WriteLine(art);
+            Console.Write(" >> 1: Users \n >> 2: Posts \n >> 3: Roles \n >>" +
+                          " 4: Tags \n >> 5: Categories \n >> 6: Exit \n >> ");
+            key = Console.ReadKey().Key;
+            
+            switch (key)
+            {
+                case ConsoleKey.D1: MenuUserScreen.Load(connection); break;
+                case ConsoleKey.D2: Load(connection); break;
+                case ConsoleKey.D3: MenuRoleScreen.Load(connection); break;
+                case ConsoleKey.D4: MenuTagScreen.Load(connection); break;
+                case ConsoleKey.D5: Load(connection); break;
+                case ConsoleKey.D6: Console.Clear(); break;
+                default: continue;
+            }
+            
+        } while (key != ConsoleKey.D1 && key != ConsoleKey.D2 && key != ConsoleKey.D3 &&
+                 key != ConsoleKey.D4 && key != ConsoleKey.D5 && key != ConsoleKey.D6);
     }
 }
