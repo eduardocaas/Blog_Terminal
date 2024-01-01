cce450b0aa73efd67fd8ad6ba37755cbf60e3092
eduardocaas <eduardofc.dev@gmail.com>
2024-01-01 13:10:38 -0300

    Refactored menu

--- Screens/UserScreen/MenuUserScreen.cs
+++ Screens/UserScreen/MenuUserScreen.cs
@@ -8,28 +8,36 @@ public class MenuUserScreen
     
     public static void Load(SqlConnection connection)
     {
-        Console.Clear();
-        Console.WriteLine(@"
+        var art = @"
               _    _   _____  ______  _____    _____ 
              | |  | | / ____||  ____||  __ \  / ____|
              | |  | || (___  | |__   | |__) || (___  
              | |  | | \___ \ |  __|  |  _  /  \___ \ 
              | |__| | ____) || |____ | | \ \  ____) |
               \____/ |_____/ |______||_|  \_\|_____/                                               
-        ");
-        Console.Write(" >> 1: Show users \n >> 2: Show users with roles \n >> 3: Create user" +
-                        "\n >> 4: Update user \n >> 5: Delete user \n >> 6: Return \n >> ");
-        short option = short.Parse(Console.ReadLine());
-
-        switch (option)
+        ";
+        
+        ConsoleKey key;
+        
+        do
         {
-            case 1: ListUserScreen.Load(1, connection); break;
-            case 2: ListUserScreen.Load(2, connection); break;
-            case 3: CreateUserScreen.Load(connection); break;
-            case 4: Load(connection); break;
-            case 5: DeleteUserScreen.Load(connection); break;
-            case 6: MenuScreen.Load(connection); break;
-            default: Load(connection); break;
-        }
+            Console.Clear();
+            Console.WriteLine(art);
+            Console.Write(" >> 1: Show users \n >> 2: Show users with roles \n >> 3: Create user" +
+                          "\n >> 4: Update user \n >> 5: Delete user \n >> 6: Return \n >> ");
+            key = Console.ReadKey().Key;
+            
+            switch (key)
+            {
+                case ConsoleKey.D1: ListUserScreen.Load(1, connection); break;
+                case ConsoleKey.D2: ListUserScreen.Load(2, connection); break;
+                case ConsoleKey.D3: CreateUserScreen.Load(connection); break;
+                case ConsoleKey.D4: Load(connection); break;
+                case ConsoleKey.D5: DeleteUserScreen.Load(connection); break;
+                case ConsoleKey.D6: MenuScreen.Load(connection); break;
+                default: continue;
+            }
+        } while (key != ConsoleKey.D1 && key != ConsoleKey.D2 && key != ConsoleKey.D3 &&
+                 key != ConsoleKey.D4 && key != ConsoleKey.D5 && key != ConsoleKey.D6);
     }
 }
