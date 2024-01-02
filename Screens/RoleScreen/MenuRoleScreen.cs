b36a11aa1317fc8ce106ccb158533fcf39e39ef9
eduardocaas <eduardofc.dev@gmail.com>
2024-01-01 22:33:59 -0300

    Refactored menu

--- Screens/RoleScreen/MenuRoleScreen.cs
+++ Screens/RoleScreen/MenuRoleScreen.cs
@@ -6,29 +6,35 @@ public static class MenuRoleScreen
 {
     public static void Load(SqlConnection connection)
     {
-        Console.Clear();
-        Console.WriteLine(@"
+        var art = @"
               _____    ____   _       ______   _____ 
              |  __ \  / __ \ | |     |  ____| / ____|
              | |__) || |  | || |     | |__   | (___  
              |  _  / | |  | || |     |  __|   \___ \ 
              | | \ \ | |__| || |____ | |____  ____) |
              |_|  \_\ \____/ |______||______||_____/                                                      
-        ");
+        ";
 
-        Console.Write(" >> 1: Show roles \n >> 2: Add role to user \n " + 
-                      ">> 3: Numbers of users per role \n >> 4: Delete role \n >> 5: Return \n >> ");
-
-        short option = short.Parse(Console.ReadLine());
-
-        switch (option)
+        ConsoleKey key;
+        
+        do
         {
-            case 1: ListRoleScreen.Load(connection); break;
-            case 2: Load(connection); break;
-            case 3: Load(connection); break;
-            case 4: Load(connection); break;
-            case 5: MenuScreen.Load(connection); break;
-            default: Load(connection); break;
-        }
+            Console.Clear();
+            Console.WriteLine(art);
+            Console.Write(" >> 1: Show roles \n >> 2: Add role to user \n " + 
+                          ">> 3: Numbers of users per role \n >> 4: Delete role \n >> 5: Return \n >> ");
+            key = Console.ReadKey().Key;
+            switch (key)
+            {
+                case ConsoleKey.D1: ListRoleScreen.Load(connection); break;
+                case ConsoleKey.D2: Load(connection); break;
+                case ConsoleKey.D3: Load(connection); break;
+                case ConsoleKey.D4: Load(connection); break;
+                case ConsoleKey.D5: MenuScreen.Load(connection); break;
+                default: continue;
+            }
+
+        } while (key != ConsoleKey.D1 && key != ConsoleKey.D2 && key != 
+                 ConsoleKey.D3 && key != ConsoleKey.D4 && key != ConsoleKey.D5);
     }
 }
--- Screens/TagScreen/MenuTagScreen.cs
+++ Screens/TagScreen/MenuTagScreen.cs
@@ -6,26 +6,32 @@ public static class MenuTagScreen
 {
     public static void Load(SqlConnection connection)
     {
-        Console.Clear();
-        Console.WriteLine(@"
+        var art = @"
               _______         _____   _____ 
              |__   __| /\    / ____| / ____|
                 | |   /  \  | |  __ | (___  
                 | |  / /\ \ | | |_ | \___ \ 
                 | | / ____ \| |__| | ____) |
                 |_|/_/    \_\\_____||_____/                      
-        ");
-        Console.Write(" >> 1: Show tags \n >> 2: Create tag \n >> 3: Add tag to post \n >> 4: Return \n >> ");
+        ";
+
+        ConsoleKey key;
         
-        short option = short.Parse(Console.ReadLine());
-        
-        switch (option)
+        do
         {
-            case 1: ListTagScreen.Load(connection); break;
-            case 2: CreateTagScreen.Load(connection); break;
-            case 3: Load(connection); break;
-            case 4: MenuScreen.Load(connection); break;
-            default: Load(connection); break;
-        }
+            Console.Clear();
+            Console.WriteLine(art);
+            Console.Write(" >> 1: Show tags \n >> 2: Create tag \n >> 3: Add tag to post \n >> 4: Return \n >> ");
+            key = Console.ReadKey().Key;
+                    
+            switch (key)
+            {
+                case ConsoleKey.D1: ListTagScreen.Load(connection); break;
+                case ConsoleKey.D2: CreateTagScreen.Load(connection); break;
+                case ConsoleKey.D3: Load(connection); break;
+                case ConsoleKey.D4: MenuScreen.Load(connection); break;
+                default: continue;
+            }
+        } while (key != ConsoleKey.D1 && key != ConsoleKey.D2 && key != ConsoleKey.D3 && key != ConsoleKey.D4);
     }
 }
