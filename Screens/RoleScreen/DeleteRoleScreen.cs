7846176c030552a8189cff017b51bf47e4693cfa
eduardocaas <eduardofc.dev@gmail.com>
2024-01-02 23:25:48 -0300

    Method load

---
+++ Screens/RoleScreen/DeleteRoleScreen.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Blog.Screens.RoleScreen;
+
+public class DeleteRoleScreen
+{
+
+    public static void Load(SqlConnection connection)
+    {
+        var art = @"
+              _____   ______  _       ______  _______  ______      _____    ____   _       ______  
+             |  __ \ |  ____|| |     |  ____||__   __||  ____|    |  __ \  / __ \ | |     |  ____| 
+             | |  | || |__   | |     | |__      | |   | |__       | |__) || |  | || |     | |__    
+             | |  | ||  __|  | |     |  __|     | |   |  __|      |  _  / | |  | || |     |  __|   
+             | |__| || |____ | |____ | |____    | |   | |____     | | \ \ | |__| || |____ | |____  
+             |_____/ |______||______||______|   |_|   |______|    |_|  \_\ \____/ |______||______|                                                                        
+        ";
+        
+        //TODO: CRIAR TRIGGER NO DB PARA DELETAR RELAÇÃO USER - ROLE
+        
+    }
+
+   
+}
