c7486112ab1b778f95fcf02c0354b261d85ac7c0
eduardocaas <eduardofc.dev@gmail.com>
2024-01-02 23:35:51 -0300

    Menu for post

---
+++ Screens/PostScreen/MenuPostScreen.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Blog.Screens.PostScreen;
+
+public class MenuPostScreen
+{
+    public static void Load(SqlConnection connection)
+    {
+        var art = @"
+              _____    ____    _____  _______  _____ 
+             |  __ \  / __ \  / ____||__   __|/ ____|
+             | |__) || |  | || (___     | |  | (___  
+             |  ___/ | |  | | \___ \    | |   \___ \ 
+             | |     | |__| | ____) |   | |   ____) |
+             |_|      \____/ |_____/    |_|  |_____/                     
+        ";
+
+        ConsoleKey key;
+        
+        do
+        {
+            Console.Clear();
+            Console.WriteLine(art);
+            Console.Write(" >> 1: Show post by");
+        } while (expression);
+    }
+}
