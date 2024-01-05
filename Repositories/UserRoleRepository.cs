7b730dde1ae18c994c046ee142b94be72b181c5c
eduardocaas <eduardofc.dev@gmail.com>
2024-01-04 23:31:49 -0300

    Created UserRoleRepository.cs, and methods for insert

---
+++ Repositories/UserRoleRepository.cs
@@ -0,0 +1,28 @@
+﻿using Blog.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Blog.Repositories;
+
+public class UserRoleRepository
+{
+    private readonly SqlConnection _connection;
+
+    public UserRoleRepository(SqlConnection connection)
+        => _connection = connection;
+
+    public int InsertUserRole(string userEmail, int roleId)
+    {
+        return 0;
+    }
+
+    public int InsertUserRole(int userId, int roleId)
+    {
+        return 0;
+    }
+
+    public int InsertUserRole(string userEmail, string roleSlug)
+    {
+        return 0;
+    }
+
+}
