fbcb24eccf8d5a2709bd25e31b64dd115163a1c6
eduardocaas <eduardofc.dev@gmail.com>
2024-01-06 18:43:21 -0300

    Create methods DeleteWithProcedure

--- Repositories/RoleRepository.cs
+++ Repositories/RoleRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace Blog.Repositories;
+﻿using Microsoft.Data.SqlClient;
+
+namespace Blog.Repositories;
 
 public class RoleRepository
 {
-    
+    private readonly SqlConnection _connection;
+
+    public RoleRepository(SqlConnection connection)
+        => _connection = connection;
+
+    public int DeleteWithProcedure(int id)
+    {
+        return 0;
+    }
+
+    public int DeleteWithProcedure(string Slug)
+    {
+        
+    }
 }
