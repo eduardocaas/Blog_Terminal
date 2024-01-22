d1a5a6ae3f5840a0683b55f70c7bc9510d9de56a
eduardocaas <eduardofc.dev@gmail.com>
2024-01-22 10:41:51 -0300

    Add UserRole.cs

---
+++ Models/UserRole.cs
@@ -0,0 +1,13 @@
+﻿namespace Blog.Models;
+
+public class UserRole
+{
+    public int UserId { get; set; }
+    public int RoleId { get; set; }
+
+    public UserRole(int userId, int roleId)
+    {
+        UserId = userId;
+        RoleId = roleId;
+    }
+}
